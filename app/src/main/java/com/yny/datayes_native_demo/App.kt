91abea954c94ad86d2a1fc823fc25910228fc477
Nianyi.Yang <nianyi.yang@datayes.com>
2020-06-19 18:51:38 +0800

    Initial commit

---
+++ app/src/main/java/com/yny/datayes_native_demo/App.kt
@@ -0,0 +1,31 @@
+package com.yny.datayes_native_demo
+
+import android.app.Application
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.embedding.engine.FlutterEngineCache
+import io.flutter.embedding.engine.dart.DartExecutor
+
+class App : Application() {
+
+    /**
+     * 缓存 FlutterEngine
+     */
+    private lateinit var flutterEngine: FlutterEngine
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Instantiate a FlutterEngine.
+        flutterEngine = FlutterEngine(this)
+
+        // Start executing Dart code to pre-warm the FlutterEngine.
+        flutterEngine.dartExecutor.executeDartEntrypoint(
+            DartExecutor.DartEntrypoint.createDefault()
+        )
+
+        // Cache the FlutterEngine to be used by FlutterActivity.
+        FlutterEngineCache
+            .getInstance()
+            .put(Constants.FlutterEngineTag, flutterEngine)
+    }
+}
---
+++ app/src/main/java/com/yny/datayes_native_demo/Constants.kt
@@ -0,0 +1,5 @@
+package com.yny.datayes_native_demo
+
+object Constants {
+    const val FlutterEngineTag = "flutter_demo"
+}
